f0b2e9c4ad805e9eb6cbae720a43c3a41e490fd7
Ascett <79991404+Ascett@users.noreply.github.com>
2022-06-09 17:32:49 +0300

    Lab_Pab
    Lab_Pab

---
+++ src/main/java/mainLab.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+public class mainLab {
+
+    private static ExecutorService es;
+    private static final int NUMBERS_TO_CHECK = 5000;
+
+    public static void main(String[] args) {
+        CheckRunner.init(NUMBERS_TO_CHECK);
+        es = Executors.newCachedThreadPool();
+        int threadCount = NUMBERS_TO_CHECK / 100 * 10;
+        IntStream.range(0, threadCount).forEach(i -> es.submit(new PrimeChecker()));
+        es.shutdown();
+    }
+
+
+}
